b4d82c352934899580414e9a8c1811dc374abbc2
Karolina Grzywa <grzywakarolina@gmail.com>
2025-11-30 09:09:54 +0100

    BuddySupportRepository

---
+++ Repositories/BuddySupportRepository.cs
@@ -0,0 +1,39 @@
+﻿using NomadBuddy00.Data;
+using NomadBuddy00.Models;
+
+namespace NomadBuddy00.Repositories
+{
+    public class BuddySupportRepository : IBuddySupportRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BuddySupportRepository(AppDbContext context)
+        {
+            _context = context;   
+        }
+        public Task AddAsync(BuddySupport support)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BuddySupport>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BuddySupport> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(BuddySupport s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
