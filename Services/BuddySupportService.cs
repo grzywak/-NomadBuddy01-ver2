23e64ed3d3fdfa4c4a484bb00a1e813ce57b41f1
Karolina Grzywa <grzywakarolina@gmail.com>
2025-12-01 15:35:32 +0100

    add repos to service

--- Services/BuddySupportService.cs
+++ Services/BuddySupportService.cs
@@ -6,11 +6,18 @@ namespace NomadBuddy00.Services
     public class BuddySupportService : IBuddySupportService
     {
         private readonly IBuddySupportRepository _supporRepository;
+        private readonly IBuddySupportRequestRepository _requestRepository;
+        private readonly IBuddySupportSessionRepository _sessionRepository;
+        private readonly IBuddySupportRatingRepository _ratingRepository;
 
-        public BuddySupportService(IBuddySupportRepository supporRepository)
+        public BuddySupportService(IBuddySupportRepository supporRepository, IBuddySupportRequestRepository requestRepository, IBuddySupportSessionRepository sessionRepository, IBuddySupportRatingRepository ratingRepository)
         {
             _supporRepository = supporRepository;
+            _requestRepository = requestRepository;
+            _sessionRepository = sessionRepository;
+            _ratingRepository = ratingRepository;
         }
+
 
         public async Task<bool> CreateAsync(BuddySupport model)
         {
@@ -38,6 +45,20 @@ namespace NomadBuddy00.Services
         public async Task<BuddySupport?> GetDetailsAsync(int supportId)
         {
                return await _supporRepository.GetByIdAsync(supportId);
+        }
+
+        //obsluga request
+        public Task<bool> SendRequestAsync(int supportId, string nomadId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> AcceptRequestAsync(int requestId, string buddyId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> RejectRequestAsync(int requestId, string buddyId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
